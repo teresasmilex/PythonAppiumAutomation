cccee60b5c229781ec4018d510792b6208ee0a35
dmanakov <dmanakov@dengisrazy.ru>
2023-04-25 01:12:05 +0300

    Week 1 Ex 2: test_get_class_number created

--- test_main.py
+++ test_main.py
@@ -2,6 +2,11 @@ import pytest
 
 
 class MainClass:
+    __class_number = 20
+
+    def get_class_number(self) -> int:
+        return self.__class_number
+
     @staticmethod
     def get_local_number() -> int:
         return 14
@@ -10,3 +15,12 @@ class MainClass:
 class TestMainClass(MainClass):
     def test_get_local_number(self):
         assert self.get_local_number() == 14, "Local number is not 14"
+
+    def test_get_class_number(self):
+        if self.get_class_number() > 45:
+            pass
+        elif self.get_class_number() == 45:
+            raise Exception("Class number is 45")
+        elif self.get_class_number() < 45:
+            raise Exception("Class number is less than 45")
+

de0e9926f979a73b8c46e5203ab62d2e8192613e
dmanakov <dmanakov@dengisrazy.ru>
2023-04-25 09:48:31 +0300

    Week 1 Ex 3: test_get_class_string created

--- test_main.py
+++ test_main.py
@@ -3,9 +3,13 @@ import pytest
 
 class MainClass:
     __class_number = 20
+    __class_string = "Hello, world"
 
     def get_class_number(self) -> int:
         return self.__class_number
+
+    def get_class_string(self) -> str:
+        return self.__class_string
 
     @staticmethod
     def get_local_number() -> int:
@@ -23,4 +27,9 @@ class TestMainClass(MainClass):
             raise Exception("Class number is 45")
         elif self.get_class_number() < 45:
             raise Exception("Class number is less than 45")
+
+    def test_get_class_string(self):
+        assert "hello" in self.get_class_string() or \
+               "Hello" in self.get_class_string(),\
+            "String doesn't contain a greeting"
 
